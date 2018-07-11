336973f4130b29d66ac944182e838537aa4e721e
Elena Lape <elena.l@hotmail.lt>
2018-07-11 18:58:46 +0300

    add the api json parser

--- microjira.py
+++ microjira.py
@@ -42,3 +42,73 @@ def api_request(username, password, url):
 		exit()
 	
 	return json.loads(result.read())
+
+# Jira REST API returns A LOT of irrelevant fields.
+# Many of them will contain no information whatsoever.
+# This is a concise list of the most important fields - 
+# it is less than the python jira library can handle
+# but for most tasks it is more than enough
+def jira_parser(result):
+    issue_list = []
+    
+    if result['total'] > 0:
+        print result['total']
+        for i in result['issues']:
+            issue = {}
+            issue['issue_id'] = str(i['key'])
+            print issue['issue_id']
+
+            # issue summary // kinda like a name
+            issue['summary'] = str(i['fields']['summary'])
+
+            # date created
+            issue['created'] = str(i['fields']['created'])[:10] + ' ' + str(i['fields']['created'])[11:19]
+
+            # date updated
+            issue['updated'] = str(i['fields']['updated'])[:10] + ' ' + str(i['fields']['updated'])[11:19]
+
+            # priority
+            issue['priority'] = str(i['fields']['priority']['name'])
+
+            # resolution if exists
+            if i['fields']['resolution']:
+                issue['resolution'] = str(i['fields']['resolution']['name'] + ' ' + str(i['fields']['resolution']['description']))
+
+            # environment (can be instance/server) where it happened if available
+            if 'environment' in i['fields'] and i['fields']['environment']:
+                issue['environment'] = str(i['fields']['environment'])
+
+            # component if such exists
+            if 'components' in i['fields']:
+                output = []
+                # ofile.write('Component: %s\n' % (i['fields']['components'][0]['name']))
+                for name in i['fields']['components']:
+                    output.append(str(name['name']))
+                issue['components'] = ','.join(output)
+
+            # affected versions
+            if 'versions' in i['fields']:
+                output = []
+                # ofile.write('Component: %s\n' % (i['fields']['components'][0]['name']))
+                for name in i['fields']['versions']:
+                    output.append(str(name['name']))
+                issue['versions_affected'] = ' ,'.join(output)
+
+            # fix versions
+            if 'fixVersions' in i['fields']:
+                output = []
+                # ofile.write('Component: %s\n' % (i['fields']['components'][0]['name']))
+                for name in i['fields']['fixVersions']:
+                    output.append(str(name['name']))
+                issue['fix_versions'] = ' ,'.join(output)
+
+            # Full description of the issue
+            # Exercise caution: can contain anything that may require
+            # further handling and parsing (eg. a Java stack or embedded html) 
+            issue['description'] = str(i['fields']['description'])
+
+            issue_list.append(issue)
+    else:
+        print 'No similar results found in JIRA.'
+
+    return issue_list
